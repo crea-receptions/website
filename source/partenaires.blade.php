002c65ff102923e97ef10e9fcd4991893c0206cc
Patrick Tournet <ptournet@users.noreply.github.com>
2023-07-11 17:01:45 +0200

    Première mise à jour des partenaires

--- source/partenaires.blade.php
+++ source/partenaires.blade.php
@@ -15,23 +15,20 @@ title_img: /assets/img/main-partenaires.png
             <x-partenaire title="Inga" subtitle="des Riaux" image="{{ $page->baseUrl }}/assets/img/content/inga.jpg" image_alt="Inga des Riaux" link="http://www.ingadesriaux.com/">
                 Amarré dans le 16ème arrondissement de Marseille, avec ses 35 mètres de long et ses 6 mètres de large, ce bateau finlandais en bois construit en 1928, vous ouvre ses portes dans une ambiance unique, chaleureuse et conviviale.
             </x-partenaire>
-            <x-partenaire mode="even" title="Chateau de" subtitle="la Beaumetane" image="{{ $page->baseUrl }}/assets/img/content/1431077523.jpg" image_alt="Chateau de la Beaumetane" link="https://www.chateau-la-beaumetane.fr/en/home/">
+            <x-partenaire mode="even" title="Chateau de" subtitle="la Beaumetane" image="{{ $page->baseUrl }}/assets/img/content/1431077523.jpg" image_alt="Chateau de la Beaumetane" link="https://www.chateau-la-beaumetane.fr/">
                 En plein cœur de la Provence, proche d’Aix, de Marseille et de Salon, entouré de vignes, Nathalie et Mike vous accueillent au Château la Beaumetane dans un cadre enchanteur. Le Château la Beaumetane est un lieu magique, chargé d'histoire, pour vos évènements privés et professionnels.
             </x-partenaire>
-            <x-partenaire title="Chateau de" subtitle="la Buzine" image="{{ $page->baseUrl }}/assets/img/content/780x480.jpg" image_alt="Chateau de la Buzine" link="https://labuzine.com/fr">
+            <x-partenaire title="Chateau de" subtitle="la Buzine" image="{{ $page->baseUrl }}/assets/img/content/780x480.jpg" image_alt="Chateau de la Buzine" link="https://labuzine.com/">
                 Pour vos séminaires, soirées, inaugurations, mariages... Faites confiance à un professionnel, Cadre magnifique sur la Valentine, Marseille. Allez-y de notre part ! Excellent accueil...
             </x-partenaire>
             <x-partenaire mode="even" title="Le Mas des" subtitle="Garguiers" image="{{ $page->baseUrl }}/assets/img/content/S135876182A.jpg" image_alt="Le Mas des Garguiers" link="http://www.mas-garguier.fr/">
                 Magnifique cadre pour vos évènements sur Gemenos! Nous officions sur ce site et sommes heureux de ce partenariat ! N'hésitez pas à vous y rendre de notre part, vous serez conquis par ce lieu !
             </x-partenaire>
-            <x-partenaire title="Domaine de" subtitle="la Sibérie" image="{{ $page->baseUrl }}/assets/img/content/17157842_1825331587716340_3973106648085093376_o.jpg" image_alt="Domaine de la Siberie" link="https://www.domaine-la-siberie.com/">
-                C'est dans une luxueuse demeure nichée au coeur de la campagne Aixoise que ce domaine se situe. Profitez d'un calme absolu, entre vignes et forêts vous pourrez admirer les jolies couleurs de la Provence. Un grand espace de jeux est prévu pour les enfants et les grands : trampoline, volley, foot... ​ Très facile d'accès, le domaine se situe à une dizaine de minutes du centre d'Aix en Provence. le stationnement se fera à l'intérieur de la propriété.
+            <x-partenaire title="Chateau de" subtitle="la Dorgonne" image="{{ $page->baseUrl }}/assets/img/content/chateau de la dorgonne.jpg" image_alt="Chateau de la Dorgonne" link="https://www.chateauladorgonne.com/">
+                Magnifique lieu sur la Tour d'Aigues De riches expériences dans l’organisation d’événements permettent à leur équipe de vous accompagner dans la réalisation de vos multiples projets : Dîners spectacles (plus de 200 personnes par soirée) Concerts de musique classiques ou modernes (plusieurs centaines de personnes par concert) Dîners d’entreprise (jusqu’à 150 personnes) Déjeuners de congrès...
             </x-partenaire>
             <x-partenaire mode="even" title="Chateau" subtitle="Grand Callamand" image="{{ $page->baseUrl }}/assets/img/content/grand_callamand.jpg" image_alt="Chateau Grand Callamand" link="http://www.chateaugrandcallamand.com/">
                 Le Château Grand Callamand accueille vos événements privés et professionnels. Domaine viti vinicole, chambres d'hôtes dans une bastide du XVI siècle située dans le Parc du Luberon face à la montagne de la Sainte Victoire. A pertuis.
-            </x-partenaire>
-            <x-partenaire title="Chateau de" subtitle="la Dorgonne" image="{{ $page->baseUrl }}/assets/img/content/chateau de la dorgonne.jpg" image_alt="Chateau de la Dorgonne" link="https://www.chateauladorgonne.com/">
-                Magnifique lieu sur la Tour d'Aigues De riches expériences dans l’organisation d’événements permettent à leur équipe de vous accompagner dans la réalisation de vos multiples projets : Dîners spectacles (plus de 200 personnes par soirée) Concerts de musique classiques ou modernes (plusieurs centaines de personnes par concert) Dîners d’entreprise (jusqu’à 150 personnes) Déjeuners de congrès...
             </x-partenaire>
         </div>
     </div>
@@ -42,7 +39,7 @@ title_img: /assets/img/main-partenaires.png
             <x-partenaire title="Palais des" subtitle="congrès Agora" image="{{ $page->baseUrl }}/assets/img/content/agora.jpg" image_alt="Palais des congrès Agora" link="http://www.agora-congres.com/">
                 Le centre de congrès AGORA bénéficie d'une situation privilégiée au coeur du triangle Aix en Provence, Marseille, Cassis bénéficiant d'une grande proximité avec tous les moyens d'accès (autoroute, TGV, avion) ainsi que des centres d'intérêts touristiques et culturels.
             </x-partenaire>
-            <x-partenaire mode="even" title="Le" subtitle="BUZ" image="{{ $page->baseUrl }}/assets/img/content/18222636_1602543539756243_6380681393446553017_n.jpg" image_alt="Le BUZ" link="http://www.buz-marseille.fr/">
+            <x-partenaire mode="even" title="Le" subtitle="BUZ" image="{{ $page->baseUrl }}/assets/img/content/18222636_1602543539756243_6380681393446553017_n.jpg" image_alt="Le BUZ" link="https://www.buz-marseille.fr/">
                 Situé sur Marseille, le Buz vous séduira de par son modernisme, son originalité, son cadre... Pour tous vos événements, professionnels ou particuliers, il saura répondre à vos attentes !
             </x-partenaire>
             <x-partenaire title="Le Moulin" subtitle="de Gemenos" image="{{ $page->baseUrl }}/assets/img/content/le-moulin-de-gemenos-mariage-pre-13.jpg" image_alt="Le Moulin de Gemenos" link="http://www.moulin-gemenos.fr/">
