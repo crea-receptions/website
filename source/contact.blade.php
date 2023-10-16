e898134729d70871177802e8435a5d32aa92a5e1
Patrick Tournet <ptournet@users.noreply.github.com>
2023-10-16 09:25:48 +0200

    Finalisation page contact

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -9,18 +9,18 @@ title_img: /assets/img/main-contact.png
 @section('pagecontent')
 <div class="container relative mx-auto max-w-1166">
     <div class="flex flex-col items-center">
-        <div class="w-4/5 sm:w-3/5 p-4 md:p-8 lg:p-10 xl:p-12 pb-3.5 bg-gray-200 mt-80px relative">
-            <div class="absolute pt-12 pb-12 pr-10 -top-144px -right-120px w-300 green-bloc3 pl-9">
-                <div class="relative mx-auto text-3xl text-center text-white pt-14 w-213 h-201 green-title">
+        <div class="w-4/5 sm:w-3/5 p-4 md:px-8 lg:px-10 xl:px-12 pb-3.5 bg-gray-200 mt-80px relative">
+            <div class="absolute py-2 sm:py-12 -top-120px sm:-top-144px -right-30px sm:-right-120px w-213 sm:w-300 green-bloc3 bg-center px-2 sm:px-10">
+                <div class="relative mx-auto text-3xl text-center text-white pt-16 w-213 h-201 green-title bg-center">
                     <p class="m-0 font-sans">demander</p>
                     <p class="mt-0 font-script">un devis</p>
-                    <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -left-80px"></div>
+                    <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -left-40px sm:-left-80px"></div>
                 </div>
             </div>
             <form action="https://formsubmit.co/4f4c7cf0d2b3477a4e7135219887c40e" method="post">
                 <div class="relative">
                     <h2 class="pt-20 text-xl font-semibold">vos<br>coordonnées</h2>
-                    <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -left-80px"></div>
+                    <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -left-65px md:-left-80px"></div>
                 </div>
                 <div class="flex flex-wrap text-xs">
                     <input type="hidden" name="_subject" value="[CREARECEPTION] Demande de devis">
@@ -61,7 +61,7 @@ title_img: /assets/img/main-contact.png
                 </div>
                 <div class="relative">
                     <h2 class="pt-12 text-xl font-semibold">votre<br/>r&eacute;ception</h2>
-                    <div class="absolute h-1.4 bg-gray-900 w-15 top-65px -left-80px"></div>
+                    <div class="absolute h-1.4 bg-gray-900 w-15 top-65px -left-65px md:-left-80px"></div>
                 </div>
                 <div class="flex flex-wrap text-xs">
                     <div class="w-full px-4 sm:w-1/2 mt-7">
@@ -98,18 +98,18 @@ title_img: /assets/img/main-contact.png
             </form>
         </div>
         <div class="relative w-4/5 mt-24 mb-7">
-            <div class="absolute pt-12 pb-12 pr-10 -bottom-144px -left-144px w-300 green-bloc3 pl-9">
+            <div class="absolute py-2 sm:py-12 px-2 sm:px-10 -bottom-100px sm:-bottom-144px -left-30px sm:-left-40px md:-left-60px lg:-left-80px xl:-left-120px w-213 sm:w-300 green-bloc3">
                 <div class="relative mx-auto text-3xl text-center text-white pt-14 w-213 h-201 green-title">
                     <p class="m-0 font-sans">notre</p>
                     <p class="mt-0 font-script">adresse</p>
-                    <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -right-80px"></div>
+                    <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -right-30px sm:-right-80px"></div>
                 </div>
             </div>
             <div class="pb-2">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11607.692823898653!2d5.59939!3d43.336796!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x93f9f83e398936e2!2zQ3LDqWEtUsOpY2VwdGlvbnM!5e0!3m2!1sfr!2sfr!4v1559301181671!5m2!1sfr!2sfr" style="border:0" allowfullscreen="" width="960" height="450" frameborder="0"></iframe>
             </div>
         </div>
-        <div class="-ml-48 text-sm text-gray-400">
+        <div class="relative -right-80px sm:-right-120px md:-right-144px text-sm text-gray-400">
             CRÉA RÉCEPTIONS<br>
             ZAC de Saint Esteve lot 20 Box 3&nbsp;<br>
             13360 ROQUEVAIRE
