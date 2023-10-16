7648026ffd39541aa7b11eac97855201656d2125
Patrick Tournet <ptournet@users.noreply.github.com>
2023-10-16 23:42:38 +0200

    Finalisation page thanks

--- source/thanks.blade.php
+++ source/thanks.blade.php
@@ -8,7 +8,7 @@ title_img: /assets/img/main-contact.png
 
 @section('pagecontent')
 <div class="container relative mx-auto max-w-1166">
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center px-4">
         <p class="mt-20 text-xl text-gray-700">Merci pour votre <span class="text-anis">demande</span>, nous vous</p><br>
         <p class="text-xl text-gray-700">re-contacterons dans les plus brefs délais.</p>
     </div>
